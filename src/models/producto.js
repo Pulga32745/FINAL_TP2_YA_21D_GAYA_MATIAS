56cfd7d038e09673ed85899371a2f62b27cff7d0
Matias Gaya <matutegaya@gmail.com>
2025-12-16 19:46:56 -0300

    Creacion models

---
+++ src/models/producto.js
@@ -0,0 +1,8 @@
+export default class Producto {
+  constructor({ id, producto, stockAmount, fechaIngreso }) {
+    this.id = id;
+    this.producto = producto;
+    this.stockAmount = stockAmount;
+    this.fechaIngreso = fechaIngreso;
+  }
+}
