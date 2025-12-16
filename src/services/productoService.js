d3722e0e85080c9af42702c4057210e4ea858133
Matias Gaya <matutegaya@gmail.com>
2025-12-16 19:54:18 -0300

    Creacion del service de producto

---
+++ src/services/productoService.js
@@ -0,0 +1,16 @@
+import { v4 as uuid } from 'uuid';
+import repo from '../repository/productoRepositoryJson.js';
+
+export default {
+  create: ({ producto, stockAmount }) => {
+    if (!producto || stockAmount < 0)
+      throw { statusCode: 400, error: 'Datos inválidos' };
+
+    return repo.save({
+      id: uuid(),
+      producto,
+      stockAmount,
+      fechaIngreso: new Date().toISOString().slice(0,10)
+    });
+  }
+};
