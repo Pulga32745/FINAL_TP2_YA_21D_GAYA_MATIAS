1375dcc573c613251a781885c69be933d4fbc1b7
Matias Gaya <matutegaya@gmail.com>
2025-12-16 20:03:55 -0300

    Completar el app.js y el index.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import morgan from 'morgan';
+import routes from './routes/productoRoutes.js';
+
+const app = express();
+
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use('/api/v1', routes);
+
+export default app;
