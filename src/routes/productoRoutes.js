f0054e6bbf33f15eaa59a096ba13c00020c8d745
Matias Gaya <matutegaya@gmail.com>
2025-12-16 20:01:46 -0300

    Creacion del routes de producto

---
+++ src/routes/productoRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import auth from '../middlewares/authMiddleware.js';
+import * as c from '../controllers/productoController.js';
+import { generateCSV } from '../services/albumsService.js';
+
+const r = express.Router();
+
+r.post('/productos', c.createProducto);
+r.get('/productos', c.getAll);
+r.get('/productos/:id', c.getById);
+r.put('/productos/:id', auth, c.update);
+r.delete('/productos/:id', auth, c.remove);
+
+r.get('/albums/csv', async (_,res)=>{
+  const csv = await generateCSV();
+  res.type('text/csv').send(csv);
+});
+
+export default r;
